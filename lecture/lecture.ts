650f8118bbedafa55e398a29a146f82f55d8d789
redsky <hyun0238@gmail.com>
2020-01-15 23:57:49 +0900

    5일차 제너릭 추가

--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -1,3 +1,38 @@
 const i:number = 1;
 console.log('hello typescript!!!');
 console.log(i);
+
+[1,2,3].forEach((item) => {
+    console.log(item)
+});
+
+
+
+class aaa{
+    a = 1;
+    b = 2;
+
+    aa(){
+        this.aa.call(this.a);
+    }
+}
+
+
+
+const bruce = {
+    name : 'Bruce'
+};
+
+const madeline = {
+    name : 'Madeline'
+};
+
+
+function update(birthYear: number, occupation: string){
+    this.birthYear = birthYear;
+    this.occupation = occupation;
+};
+
+update.call(bruce, 1949, 'singer'); // bruce 변경
+
+ 
